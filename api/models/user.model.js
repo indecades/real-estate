0934c4d8f2e189c72b0dbe680090e6b6a72c6e33
Rochelle Mans <123575708+indecades@users.noreply.github.com>
2023-10-17 15:50:15 +0200

    update the header and make profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        default: "https://cdn.vectorstock.com/i/preview-1x/71/90/blank-avatar-photo-icon-design-vector-30257190.jpg"
     },
 }, {timestamps: true})
 // sort information by using timestamp (e.g. latest information)
