1653e95ffa26c1fa3f18b53332ed2f6d0f923715
Rochelle Mans <123575708+indecades@users.noreply.github.com>
2023-10-19 13:55:47 +0200

    ceate user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,37 @@
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from "bcryptjs"
+import User from "../models/user.model.js"
+
 // req client information and get response from server
 export const test = (req, res) => {
     res.json({
         message: "Hello World!"
     })
+}
+
+export const updateUser = async(req,res,next) => {
+    if (req.user.id !== req.params.id) {
+        next(errorHandler(401, "You can only update your own account!"))
+    }
+    try {
+        if(req.body.password){
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updateUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: { // dont req.body hacker can give full admin to themselves
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+
+        } , {new: true} // save new user information
+            )
+            const {password, ...rest} = updateUser._doc
+
+            res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
