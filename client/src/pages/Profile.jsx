6475d78d46b0b9cffffbebb8b9e406893b95c3bc
Rochelle Mans <123575708+indecades@users.noreply.github.com>
2023-10-19 16:11:19 +0200

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,30 @@
-import { useSelector, useDispatch} from "react-redux";
-import { useRef, useState, useEffect} from "react";
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { useSelector, useDispatch } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytesResumable,
+} from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
+import {
+	updateUserStart,
+	updateUserSuccess,
+	updateUserFailure,
+	deleteUserFailure,
+	deleteUserStart,
+	deleteuserSuccess,
+} from "../redux/user/userSlice.js";
 
 export default function Profile() {
 	const fileRef = useRef(null);
 	const { currentUser, loading, error } = useSelector((state) => state.user);
 	const [file, setFile] = useState(undefined);
-	const [filePerc, setFilePerc] = useState(0)
-	const [fileUploadError, setFileUploadError] = useState(false)
-	const [formData, setFormData] = useState({})
-	const [updateSuccess, setUpdateSuccess] = useState(false)
-	const dispatch = useDispatch()
+	const [filePerc, setFilePerc] = useState(0);
+	const [fileUploadError, setFileUploadError] = useState(false);
+	const [formData, setFormData] = useState({});
+	const [updateSuccess, setUpdateSuccess] = useState(false);
+	const dispatch = useDispatch();
 
 	// firebase storage rules:
 	// allow read;
@@ -20,62 +32,84 @@ export default function Profile() {
 	// request.resource.size < 2 * 1024 * 1024 &&
 	// request.resource.contentType.matches('image/.*')
 
-  useEffect(()=>{
-    if(file){
-      handleFileUpload(file)
-    }
-  },[file])
-
-const handleFileUpload = (file) => {
-const storage = getStorage(app)
-const fileName = new Date().getTime() + file.name // unique file name
-const storageRef = ref(storage, fileName)
-const uploadTask = uploadBytesResumable(storageRef, file)
-
-uploadTask.on('state_changed',
-(snapshot) => {
-	const progress = (snapshot.bytesTransferred /
-	snapshot.totalBytes) * 100
-	setFilePerc(Math.round(progress))
-},
-(error) => {
-	setFileUploadError(true)
-},
-()=>{
-	getDownloadURL(uploadTask.snapshot.ref).then
-	((downloadURL) => 
-		setFormData({...formData, avatar: downloadURL}))
-}
-)
-}
-const handleChange = (e) => {
-	setFormData({...formData, [e.target.id]: e.target.value})
-}
-
-const handleSubmit = async(e) => {
-	e.preventDefault()
-	try {
-		dispatch(updateUserStart())
-		const res = await fetch (`api/user/update/${currentUser._id}`, {
-			method: "POST",
-			headers: {"Content-Type": "application/json"},
-			body: JSON.stringify(formData)
-		})
-		const data = await res.json()
-		if(data.success === false) {
-			dispatch(updateUserFailure(data.message))
-			return
+	useEffect(() => {
+		if (file) {
+			handleFileUpload(file);
 		}
-		dispatch(updateUserSuccess(data))
-		setUpdateSuccess(true)
-	} catch (error) {
-		dispatch(updateUserFailure(error.message))
-	}
-}
+	}, [file]);
+
+	const handleFileUpload = (file) => {
+		const storage = getStorage(app);
+		const fileName = new Date().getTime() + file.name; // unique file name
+		const storageRef = ref(storage, fileName);
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		uploadTask.on(
+			"state_changed",
+			(snapshot) => {
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				setFilePerc(Math.round(progress));
+			},
+			(error) => {
+				setFileUploadError(true);
+			},
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+					setFormData({ ...formData, avatar: downloadURL })
+				);
+			}
+		);
+	};
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			dispatch(updateUserStart());
+			const res = await fetch(`api/user/update/${currentUser._id}`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(updateUserFailure(data.message));
+				return;
+			}
+			dispatch(updateUserSuccess(data));
+			setUpdateSuccess(true);
+		} catch (error) {
+			dispatch(updateUserFailure(error.message));
+		}
+	};
+
+	const handleDeleteUser = async () => {
+		try {
+			dispatch(deleteUserStart())
+			const res = fetch(`/api/user/delete/${currentUser._id}`,{
+				method: "DELETE",
+			})
+			const data = (await res).json()
+			if(data.success === false) {
+				dispatch(deleteUserFailure(error.message))
+				return
+			}
+			dispatch(deleteuserSuccess(data))
+		} catch (error) {
+			dispatch(deleteUserFailure(error.message));
+		}
+	};
+
 	return (
 		<div className="p-3 max-w-lg mx-auto">
 			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+			<form
+				onSubmit={handleSubmit}
+				className="flex flex-col gap-4"
+			>
 				<input
 					onChange={(e) => setFile(e.target.files[0])}
 					type="file"
@@ -91,23 +125,16 @@ const handleSubmit = async(e) => {
 				/>
 				<p className="text-sm self-center">
 					{fileUploadError ? (
-					<span className="text-red-700">
-						Error Image Upload (image must be less than 2mb)
-					</span>
-					) :
-					filePerc > 0 && filePerc < 0 ? (
-					<span className="text-slate-700">
-						{`Uploading ${filePerc}%`}
-					</span>
-					):
-					filePerc === 100 ? (
-						<span className="text-green-700">
-						Image successfully uploaded!
+						<span className="text-red-700">
+							Error Image Upload (image must be less than 2mb)
 						</span>
-						) : (
+					) : filePerc > 0 && filePerc < 0 ? (
+						<span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+					) : filePerc === 100 ? (
+						<span className="text-green-700">Image successfully uploaded!</span>
+					) : (
 						""
-					)
-				}
+					)}
 				</p>
 				<input
 					type="text"
@@ -132,18 +159,26 @@ const handleSubmit = async(e) => {
 					className="border p-3 rounded-lg"
 					onChange={handleChange}
 				/>
-				<button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+				<button
+					disabled={loading}
+					className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+				>
 					{loading ? "Loading..." : "Update"}
 				</button>
 			</form>
 			<div className="flex justify-between mt-5">
-				<span className="text-red-700 cursor-pointer">Delete Account</span>
+				<span
+					onClick={handleDeleteUser}
+					className="text-red-700 cursor-pointer"
+				>
+					Delete Account
+				</span>
 				<span className="text-red-700 cursor-pointer">Sign out</span>
 			</div>
-			<p className="text-red-700 mt-5">
-				{error ? error : ""}
+			<p className="text-red-700 mt-5">{error ? error : ""}</p>
+			<p className="text-green-700">
+				{updateSuccess ? "User is updated successfully!" : ""}
 			</p>
-			<p className="text-green-700">{updateSuccess ? "User is updated successfully!" : ""}</p>
 		</div>
 	);
 }
