b18c2f11d041328f07fa724c814ec91f34ae15a7
Rochelle Mans <123575708+indecades@users.noreply.github.com>
2023-10-21 14:04:02 +0200

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
 	signoutUserSuccess,
 	signoutUserFailure
 } from "../redux/user/userSlice.js";
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
 	const fileRef = useRef(null);
@@ -183,6 +184,9 @@ const handleSignOut = async() => {
 				>
 					{loading ? "Loading..." : "Update"}
 				</button>
+				<Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+				Create Listing
+				</Link>
 			</form>
 			<div className="flex justify-between mt-5">
 				<span
