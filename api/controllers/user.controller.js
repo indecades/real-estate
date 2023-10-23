e453e3f22e31add62d3046dd624b3ed78475dd50
Rochelle Mans <123575708+indecades@users.noreply.github.com>
2023-10-23 18:11:04 +0200

    Add contact landlord functionality to listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,63 +1,80 @@
-import { errorHandler } from "../utils/error.js"
-import bcryptjs from "bcryptjs"
-import User from "../models/user.model.js"
-import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 // req client information and get response from server
 export const test = (req, res) => {
-    res.json({
-        message: "Hello World!"
-    })
-}
+	res.json({
+		message: "Hello World!",
+	});
+};
 
-export const updateUser = async(req,res,next) => {
-    if (req.user.id !== req.params.id) {
-        next(errorHandler(401, "You can only update your own account!"))
-    }
-    try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password, 10)
-        }
+export const updateUser = async (req, res, next) => {
+	if (req.user.id !== req.params.id) {
+		next(errorHandler(401, "You can only update your own account!"));
+	}
+	try {
+		if (req.body.password) {
+			req.body.password = bcryptjs.hashSync(req.body.password, 10);
+		}
 
-        const updateUser = await User.findByIdAndUpdate(req.params.id,{
-            $set: { // dont req.body hacker can give full admin to themselves
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar,
-            }
+		const updateUser = await User.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: {
+					// dont req.body hacker can give full admin to themselves
+					username: req.body.username,
+					email: req.body.email,
+					password: req.body.password,
+					avatar: req.body.avatar,
+				},
+			},
+			{ new: true } // save new user information
+		);
+		const { password, ...rest } = updateUser._doc;
 
-        } , {new: true} // save new user information
-            )
-            const {password, ...rest} = updateUser._doc
-
-            res.status(200).json(rest)
-    } catch (error) {
-        next(error)
-    }
-}
+		res.status(200).json(rest);
+	} catch (error) {
+		next(error);
+	}
+};
 
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.id) 
-    return next(errorHandler(401, 'You can only delete your own account!'))
-    try {
-        await User.findByIdAndDelete(req.params.id);
-        res.clearCookie('access_token');
-        res.status(200).json("User has been deleted!");
-    } catch (error) {
-        next(error)
-    }
-}
+	if (req.user.id !== req.params.id)
+		return next(errorHandler(401, "You can only delete your own account!"));
+	try {
+		await User.findByIdAndDelete(req.params.id);
+		res.clearCookie("access_token");
+		res.status(200).json("User has been deleted!");
+	} catch (error) {
+		next(error);
+	}
+};
 
-export const getUserListings = async (req, res ,next) => {
-    if (req.user.id === req.params.id) {
-        try {
-            const listings = await Listing.find({userRef: req.params.id})
-            res.status(200).json(listings)
-        } catch (error) {
-            next(error)
-        }
-    } else {
-        return next(errorHandler(401, 'You can only view your own listings!'))
-    }
-}
+export const getUserListings = async (req, res, next) => {
+	if (req.user.id === req.params.id) {
+		try {
+			const listings = await Listing.find({ userRef: req.params.id });
+			res.status(200).json(listings);
+		} catch (error) {
+			next(error);
+		}
+	} else {
+		return next(errorHandler(401, "You can only view your own listings!"));
+	}
+};
+
+export const getUser = async (req, res, next) => {
+	try {
+		const user = await User.findById(req.params.id);
+
+		if (!user) return next(errorHandler(404, "User not found!"));
+
+		const { password: pass, ...rest } = user._doc;
+
+		res.status(200).json(rest);
+	} catch (error) {
+		next(error);
+	}
+};
